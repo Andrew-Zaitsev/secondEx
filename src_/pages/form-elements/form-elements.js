daa7d83db44e5c4848e8e1a2e412451362f8248d
Andrew <andrew_lin@mail.ru>
2020-10-20 16:50:05 +0300

    Replaced and refactored date-dropdown and filter-date-dropdown modules, made minor changes

--- src_/pages/form-elements/form-elements.js
+++ src_/pages/form-elements/form-elements.js
@@ -1,4 +1,8 @@
 import '../../modules/dropdown/dropdown.js';
 import '../../assets/js/jquery-in-parcel.js';
+
 //import '../../modules/text-field/text-field.js'
 //поработать с ошибкой в консоли, переменная pos не определена
+
+import '../../modules/date-dropdown/date-dropdown.js';
+import '../../modules/filter-date-dropdown/filter-date-dropdown.js'
