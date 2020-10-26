5eb1d06c0d815b125c021c19743ed36aa0040fdb
Andrew <andrew_lin@mail.ru>
2020-10-26 13:41:31 +0300

    Replaced and refactored bullet-list, pagination, review, room-info modules. Minor changes

--- src_/pages/form-elements/form-elements.js
+++ src_/pages/form-elements/form-elements.js
@@ -9,3 +9,5 @@ import '../../modules/filter-date-dropdown/filter-date-dropdown.js';
 import '../../modules/expandable-checkbox-list/expandable-checkbox-list.js';
 import '../../modules/like-button/like-button.js';
 import '../../modules/range-slider/range-slider.js';
+import '../../modules/pagination/pagination.js';
+import '../../modules/review/review.js';
