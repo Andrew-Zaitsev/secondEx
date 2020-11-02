50123e307e0e865ddc0ed7d59ea78a7e87548932
Andrew <andrew_lin@mail.ru>
2020-11-02 15:54:28 +0300

    Started making cards page, Made layouts of 4 cards (card-filter, card-registration, card-booking and card-sign-in)

---
+++ src/modules/card-filter/card-filter.js
@@ -0,0 +1,2 @@
+import '../../modules/date-dropdown/date-dropdown.js';
+import '../../modules/dropdown/dropdown.js';
---
+++ src/pages/cards/cards.js
@@ -0,0 +1,4 @@
+import '../../assets/js/jquery-in-parcel.js';
+import '../../modules/card-filter/card-filter.js';
+import '../../modules/card-registration/card-registration.js';
+import '../../modules/card-booking/card-booking.js';
