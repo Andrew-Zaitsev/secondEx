4930dedd5fbec4030637919fc168c2507c206971
Andrew <andrew_lin@mail.ru>
2020-11-07 21:56:26 +0300

    Started making a room-card module. Partially made it's layout. Desided to use 'lightSlider' plagin for this module slider

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -3,3 +3,4 @@ import '../../modules/card-filter/card-filter.js';
 import '../../modules/card-registration/card-registration.js';
 import '../../modules/card-booking/card-booking.js';
 import '../../modules/date-dropdown/date-dropdown.js';
+import '../../modules/room-card/room-card.js';
