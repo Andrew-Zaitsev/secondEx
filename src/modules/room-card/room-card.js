e293f3268faeda8af380ab57c6c13f22f4f5d2d5
Andrew <andrew_lin@mail.ru>
2020-11-18 15:42:52 +0300

    Connected lightSlider plagin for room-card module, added second room-card  to cards.pug

--- src/modules/room-card/room-card.js
+++ src/modules/room-card/room-card.js
@@ -1,3 +1,5 @@
+import '../../assets/js/plagins/lightSlider/lightslider.js';
+
 import {
   getNoun
 } from '../../assets/js/functions.js';
@@ -5,6 +7,27 @@ import {
 import {
   reviewNouns
 } from '../../assets/js/variables.js';
+
+$(document).ready(function () {
+  $(".room-card__slider").lightSlider({
+    item: 1,
+    enableTouch: true,
+    enableDrag: true,
+    freeMove: true,
+    mode: 'slide',
+    speed: 1000,
+    auto: false,
+    pauseOnHover: false,
+    loop: false, //works not correcly if 'true'
+    slideEndAnimation: true,
+    keyPress: true,
+    //prevHtml:,
+    //nextHtml:,
+    pager: true,
+    gallery: false,
+    currentPagerPosition: 'middle', //works not correcly if 'true'
+  });
+});
 
 const roomCardElement = document.querySelectorAll('.room-card');
 
